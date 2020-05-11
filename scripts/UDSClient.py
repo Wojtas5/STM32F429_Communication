1ffaa2fb1901ce6a77004c41f5bfe9a4243f9929
Wojciech Sadzik <wojciech.sadzik@altenpolska.pl>
2020-05-11 10:48:35 +0200

    Add Stopwatch functionality to UDS module

--- scripts/UDSClient.py
+++ scripts/UDSClient.py
@@ -32,13 +32,17 @@ def fprintTimefromHex(secs):
     print('{:02d}:{:02d}:{:02d}'.format(int(h), int(m), int(s)))
 
 
+def printStopWatchTime(payload):
+    print("Time: {0}:{1}:{2}:{3}".format(payload[4], payload[5], payload[6], payload[7]*10))
+
+
 def int_from_bytes(input):
     return int.from_bytes(input[3:5], byteorder='big', signed=True)
 
 
 # Addresses of microcontroller and PC
 destaddress = '192.168.0.10'
-srcaddress  = '192.168.0.11'
+srcaddress = '192.168.0.11'
 
 # Name of network interface card
 eth = 'Realtek PCIe GBE Family Controller'
@@ -46,23 +50,37 @@ eth = 'Realtek PCIe GBE Family Controller'
 # Service SIDs
 TesterPresentSID = b'\x3E'
 ECUResetSID = b'\x11'
+
 ReadDataByIndentifierSID = b'\x22'
 TimeFromStartupDID = b'\x01\x05'
+
+RoutineControlSID = b'\x31'
+StopWatchRoutine = b'\x13\x01'
+StopWatchStart = b'\x01'
+StopWatchStop = b'\x02'
+StopWatchSend = b'\x03'
 
 # Other SIDs
 DiagnosticSessionControlSID = b'\x10'
 TesterPresentInvalidSID = b'\x3E\xFF\xFF'
 ECUResetInvalidSID = b'\x11\x3E'
 InvalidDID = b'\x01\x06'
+InvalidRoutineSubfunc = b'\x04'
 
 # Response SIDs
 TesterPresentRPSID = b'\x7E\x3E'
 DiagnosticSessionControlRPSID = b'\x7E\x50'
 ECUResetRPSID = b'\x51\x11'
+StopWatchStartRP = b'\x71\x01\x13\x01'
+StopWatchStopRP = b'\x71\x02\x13\x01'
+StopWatchSendRP = b'\x71\x03\x13\x01'
 
 TesterPresentInvalidRPSID = b'\x7F\x3E\x13'
 ECUResetInvalidRPSID = b'\x7F\x11\x13'
 InvalidRPDID = b'\x7F\x22\x13'
+InvalidRoutineRPSID = b'\x7F\x31\x13'
+RoutineControlSeqErrorRPSID = b'\x7F\x31\x24'
+RoutineControlSubfuncNotSupported = b'\x7F\x31\x12'
 
 # Test different SIDs
 if __name__ == '__main__':
@@ -78,7 +96,13 @@ if __name__ == '__main__':
     sendUDSPacketandPrint(TesterPresentSID)
 
     # Print formatted time
-    fprintTimefromHex(int_from_bytes(sendUDSPacketandPrint(ReadDataByIndentifierSID+TimeFromStartupDID)))
+    fprintTimefromHex(int_from_bytes(sendUDSPacketandPrint(ReadDataByIndentifierSID + TimeFromStartupDID)))
     time.sleep(1)
-    fprintTimefromHex(int_from_bytes(sendUDSPacketandPrint(ReadDataByIndentifierSID+TimeFromStartupDID)))
-    sendUDSPacketandPrint(ReadDataByIndentifierSID+InvalidDID)
+    fprintTimefromHex(int_from_bytes(sendUDSPacketandPrint(ReadDataByIndentifierSID + TimeFromStartupDID)))
+    sendUDSPacketandPrint(ReadDataByIndentifierSID + InvalidDID)
+
+    sendUDSPacketandPrint(RoutineControlSID + StopWatchStart)
+    sendUDSPacketandPrint(RoutineControlSID + StopWatchStart + StopWatchRoutine)
+    sendUDSPacketandPrint(RoutineControlSID + StopWatchStart + StopWatchRoutine)
+    sendUDSPacketandPrint(RoutineControlSID + StopWatchStop + StopWatchRoutine)
+    printStopWatchTime(sendUDSPacketandPrint(RoutineControlSID + StopWatchSend + StopWatchRoutine))
--- scripts/UDSTest.py
+++ scripts/UDSTest.py
@@ -6,15 +6,28 @@ if __name__ == '__main__':
     suite1 = Suite('UDSTest')
     suite1.addTest('Services')
     suite1.EQ(TesterPresentRPSID, sendUDSPacketandPrint(TesterPresentSID))
-    suite1.NE(InvalidRPDID, sendUDSPacketandPrint(ReadDataByIndentifierSID+TimeFromStartupDID))
-    suite1.EQ(ECUResetRPSID , sendUDSPacketandPrint(ECUResetSID))
+    suite1.NE(InvalidRPDID, sendUDSPacketandPrint(ReadDataByIndentifierSID + TimeFromStartupDID))
+    suite1.EQ(ECUResetRPSID, sendUDSPacketandPrint(ECUResetSID))
     time.sleep(3)
+    suite1.EQ(StopWatchStartRP, sendUDSPacketandPrint(RoutineControlSID + StopWatchStart + StopWatchRoutine))
+    suite1.EQ(RoutineControlSeqErrorRPSID, sendUDSPacketandPrint(RoutineControlSID + StopWatchStart + StopWatchRoutine))
+    time.sleep(0.5)
+    suite1.NE(RoutineControlSeqErrorRPSID, sendUDSPacketandPrint(RoutineControlSID + StopWatchSend + StopWatchRoutine))
+
+    printStopWatchTime(sendUDSPacketandPrint(RoutineControlSID + StopWatchSend + StopWatchRoutine))
+
+    suite1.EQ(StopWatchStopRP, sendUDSPacketandPrint(RoutineControlSID + StopWatchStop + StopWatchRoutine))
+    suite1.NE(RoutineControlSeqErrorRPSID, sendUDSPacketandPrint(RoutineControlSID + StopWatchSend + StopWatchRoutine))
+
+    printStopWatchTime(sendUDSPacketandPrint(RoutineControlSID + StopWatchSend + StopWatchRoutine))
 
     suite1.addTest('Errors')
     suite1.NE(DiagnosticSessionControlRPSID, sendUDSPacketandPrint(DiagnosticSessionControlSID))
     suite1.EQ(TesterPresentInvalidRPSID, sendUDSPacketandPrint(TesterPresentInvalidSID))
     suite1.EQ(ECUResetInvalidRPSID, sendUDSPacketandPrint(ECUResetInvalidSID))
     suite1.EQ(InvalidRPDID, sendUDSPacketandPrint(ReadDataByIndentifierSID + InvalidDID))
+    suite1.EQ(RoutineControlSeqErrorRPSID, sendUDSPacketandPrint(RoutineControlSID + StopWatchStop + StopWatchRoutine))
+    suite1.EQ(InvalidRoutineRPSID, sendUDSPacketandPrint(RoutineControlSID + InvalidRoutineSubfunc + StopWatchRoutine))
+    suite1.EQ(RoutineControlSubfuncNotSupported, sendUDSPacketandPrint(RoutineControlSID + StopWatchRoutine))
 
     TEST(suite1)
-
